31e248750a5afc836335e5a8e1e4190992c675af
Stutiagrawal10 <stutiagrawal33@gmail.com>
2023-03-29 19:06:57 +0530

    added button and buildings of landing page

--- pages/components/herosection.js
+++ pages/components/herosection.js
@@ -1,6 +1,22 @@
+import Image from "next/image"
+
 function HeroSection() {
     return (
-        <div>Securing India's Property Rights Together.</div>
+        <>
+            <div className="heading">Securing India's <br/> Property Rights Together.</div>
+            <div className="button-div">
+                <button className="button">GET STARTED</button>
+            </div>
+            <div>
+                <Image
+                    src="buildings.svg"
+                    quality={100}
+                    layout = "fill"
+                    objectFit="cover"
+                    alt=""
+                />
+            </div>
+        </>
     )
 }
 
