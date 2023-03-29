84394816a47151768f16c9e48ac121267ca396cf
Pratikshaekbote <80769944+Pratikshaekbote@users.noreply.github.com>
2023-03-29 23:32:25 +0530

    navber addded

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 // import { Inter } from 'next/font/google'
 import HeroSection from "./components/herosection"
 import Image from "next/image"
+
 // const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
         alt=""
       />
       <HeroSection/>
+      
     </>
   )
 }
